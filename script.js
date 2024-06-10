1dd08143aa3fd8bcde16e998af5fcabef87eb04b
Luis Felipe <llipeandrade34@gmail.com>
2024-06-10 10:34:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ document.getElementById('calcForm').addEventListener('submit', function(event) {
     const oddempate = parseFloat(document.getElementById('oddempate').value);
     const odd1 = parseFloat(document.getElementById('odd1').value);
 
-    function calcular_proporcao(xy, odd1, oddempate) {
+    function calcular_proporcao(xy, oddml, oddempate) {
         const a = 2 + oddempate;
-        const b = odd1;
+        const b = oddml;
         const c = 100;
 
-        const x = (xy * odd1 + c) / (2 * oddempate + odd1);
+        const x = (xy * oddml + c) / (2 * oddempate + oddml);
         const y = xy - x;
 
         return { x, y };
     }
 
-    const { x, y } = calcular_proporcao(xy, odd1, oddempate);
+    const { x, y } = calcular_proporcao(xy, oddml, oddempate);
 
     document.getElementById('resultEmpate').textContent = `Stake do empate = ${x.toFixed(2)}`;
     document.getElementById('resultVitoria').textContent = `Stake da vitória = ${y.toFixed(2)}`;
